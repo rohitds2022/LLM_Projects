63a0319f56acf6358d5574f930b8ebbc103f1927
rohitds2022 <rohitds2022@gmail.com>
2023-09-27 18:30:50 +0530

    app

--- Ask_Multiple_PDFs/app.py
+++ Ask_Multiple_PDFs/app.py
@@ -1,5 +1,94 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(text = text_chunks, embedding = embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vectorstore.as_retriever();
+        memory=memory
+    )
+    return conversation_chain
+
 def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with PDFs",page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    st.header("Chat with PDFs :books:")
+    st.text_input("Ask a question")
+
+    conversation = []
+    counter = 0
     
+    while True:
+        
+        user_input = st.text_input("You:", key=f'user_input_{counter}') 
+            
+        if user_input:
+            conversation.append({"role":"user","content": user_input})
+            response = st.session_state.conversation(user_input)
+            conversation.append({"role":"model","content": response})
+            
+            st.write(f"{conversation[-1]['role']}: \n {conversation[-1]['content']}")
+            
+            counter+=1
+            
+        else:
+            break
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDFs",accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):    
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                
+                # get the text chunks
+                text_chunks= get_text_chunks(raw_text)
+                
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+    st.session_state.conversation 
 
 if __name__ =='__main__':
     main()
